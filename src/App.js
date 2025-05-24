677d039067543ff19e59f99d99034d7354b861fd
Yasemin Demirpolat <yasemindemirpolat11@gmail.com>
2025-05-24 22:45:48 +0300

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import MentorProfile from './pages/MentorProfile';
+import MenteeProfile from './pages/MenteeProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mentor-profile" element={<MentorProfile />} />
+        <Route path="/mentee-profile" element={<MenteeProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
